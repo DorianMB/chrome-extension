f9ff06708db9ca987cf6c252c9ff866efe804634
Dorian <dorian.marques.2e7@gmail.com>
2020-12-16 16:46:20 +0100

    trying to reset name

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import "./App.scss";
 
-export class App extends React.Component<{}> {
+let name: any = 'Hello !';
+
+export class App extends React.Component {
   public componentDidMount() {
     if (chrome && chrome.tabs) {
       chrome.tabs.query({ currentWindow: true, active: true }, tabs => {
         const tab = tabs[0];
-        chrome.tabs.sendMessage(tab.id || 0, { from: "popup", subject: "getFullName" }, response => {
-          console.log(response);
+        chrome.tabs.sendMessage(tab.id || 0, { from: "popup", subject: tab }, response => {
+          name = response.name;
+          console.log(name);
         });
       });
     }
   }
 
   render() {
-    return <div className="app">Hello world</div>;
+    return <div className="app">{name}</div>;
   }
 }
 
--- src/scripts/linkedin.ts
+++ src/scripts/linkedin.ts
@@ -1,10 +1,17 @@
 import { Result } from "../models/result";
 
 chrome.runtime.onMessage.addListener(async (msg, sender, response) => {
-  const result: Result = {
-    message: "Hello"
-  };
+  let res  = null;
+  if (msg.subject.status === "complete") {
+    res = document.getElementsByClassName("inline t-24 t-black t-normal break-words");
+    const result = {
+      message: msg,
+      name: res[0].innerHTML.toString().replace("[\\r\\n]+", ""),
+      document: document
+    };
+    console.log('lol', res[0].innerHTML.toString().replace("[\\r\\n]+", ""), document);
+    response(result);
+  }
 
-  response(result.message);
   return true;
 });
